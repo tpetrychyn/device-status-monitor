38a74edfa9a01799d73807de5b5fafcfc64b13af
Unknown <mlmogentale@gmail.com>
2017-11-04 13:58:38 -0600

    fs windows ting

--- webpack.config.js
+++ webpack.config.js
@@ -9,46 +9,49 @@ module.exports = {
     filename: '[name].bundle.js',
     publicPath: "dist/"
   },
+  node: {
+    fs: "empty"
+  },
   module: {
     loaders: [{
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-      // inline base64 URLs for <=8k images, direct URLs for the rest
-      {
-        test: /\.(png|jpg)$/,
-        loader: 'url-loader?limit=8192'
-      },
-      // helps to load bootstrap's css.
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&minetype=application/font-woff'
-      },
-      {
-        test: /\.woff2$/,
-        loader: 'url?limit=10000&minetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&minetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&minetype=image/svg+xml'
-      }
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    },
+    {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    },
+    {
+      test: /\.html$/,
+      loader: 'html-loader'
+    },
+    // inline base64 URLs for <=8k images, direct URLs for the rest
+    {
+      test: /\.(png|jpg)$/,
+      loader: 'url-loader?limit=8192'
+    },
+    // helps to load bootstrap's css.
+    {
+      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&minetype=application/font-woff'
+    },
+    {
+      test: /\.woff2$/,
+      loader: 'url?limit=10000&minetype=application/font-woff'
+    },
+    {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&minetype=application/octet-stream'
+    },
+    {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file'
+    },
+    {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&minetype=image/svg+xml'
+    }
     ]
   },
   plugins: [
