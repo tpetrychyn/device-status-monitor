6c912912e5498c0888ad68fc7dc60df050dcc871
Unknown <mlmogentale@gmail.com>
2017-11-05 11:10:31 -0600

    Fixin shi

--- public/components/device/device-create/device-create.controller.js
+++ public/components/device/device-create/device-create.controller.js
@@ -10,14 +10,16 @@ function deviceCreate() {
     }
 }
 
-function DeviceCreateController($scope, $state, $rootScope) {
+function DeviceCreateController($scope, $state, $rootScope, $http) {
+    $scope.device = {};
     $scope.page = $state.current.name;
     $scope.environment = process.env.NODE_ENV;
     $scope.profile = $rootScope.profile;
 
     $scope.addDevice = function () {
+        console.log($scope.device.name);
         $http.post('/api/devices', data).success(function (data, status, headers) {
-            $scope.devices = data;
+            $scope.device = data;
         })
             .error(function (data, status, headers) {
                 $scope.ResponseDetails = "Data: " + data + "Status: " + status + "Headers: " + headers;
